79bfcc8f3945bb3333e0075b4f443b513d3365dc
vincetroid <vince_trance@hotmail.com>
2019-08-22 13:34:50 -0500

    Puesta en marcha

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import fns from './utils/helpers';
+
+class App extends Component {
+
+    componentDidMount() {
+        console.log(89)
+    }
+
+    render() {
+        return (
+            <div>gjhgjh</div>
+        );
+    }
+}
+
+export default App;
